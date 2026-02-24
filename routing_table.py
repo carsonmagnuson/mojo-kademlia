2328a4287cef9409e73e1ccc35c418decb67499c
carsonmagnuson <carsonmagnuson@gmail.com>
2026-02-23 20:16:30 -0500

    add more to add def

--- routing_table.py
+++ routing_table.py
@@ -1,11 +1,13 @@
-import kbucket
+import kbucket, node_id
 
 class RoutingTable:
 
     def __init__(self):
         self.buckets = [kbucket]*256
+        self.node = node_id.NodeID("seed")
 
-    def add(self, nodeid):
+    def add(self, to_add):
+        distance = node_id.NodeID.distance(self.node.id, to_add.id)
         return
 
 
